2d72e3b02b92d29870a9715667b6a522e8ebb924
Oren Shoham <oren.avraham.shoham@gmail.com>
2021-01-03 11:21:14 -0500

    swap out rollup for parcel b/c rollup is having trouble with the recast-detour package

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Responsive Eye
 
-This site is using codekit to bundle sass and any npm dependencies. If you plan on contributing to the repo, please let me know and we can set up an alternate method for compiling resources. **Please do not make any changes directly to the CSS file until we've done that - they will get overwritten.**
+This site is using parcel to bundle sass and any npm dependencies. Parcel is configured to compile the `src` folder to a `dist` folder which is excluded from git.
+
+To develop the site on your own machine, run `npm install`, then `npm start`, and open [localhost:3000](http://localhost:3000) in your browser.
+
+To build the site, run `npm run build`.
--- src/scripts/3d-overlay/scene.js
+++ src/scripts/3d-overlay/scene.js
@@ -2,7 +2,7 @@
 
 import * as OIMO from 'oimo';
 import { Scene } from '@babylonjs/core/scene';
-import { Vector3, Color3, Color4, Angle, Axis } from '@babylonjs/core/Maths/math';
+import { Vector3, Color3, Color4, Angle, Axis, Matrix } from '@babylonjs/core/Maths/math';
 import { OimoJSPlugin } from '@babylonjs/core/Physics/Plugins/oimoJSPlugin';
 import { Animation } from '@babylonjs/core/Animations/animation';
 import { AnimationPropertiesOverride } from '@babylonjs/core/Animations/animationPropertiesOverride';
@@ -13,6 +13,11 @@ import { PhysicsImpostor } from '@babylonjs/core/Physics/physicsImpostor';
 import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial';
 import { MultiMaterial } from '@babylonjs/core/Materials/multiMaterial';
 import { SceneLoader } from '@babylonjs/core/Loading/sceneLoader';
+import { RecastJSPlugin } from '@babylonjs/core/Navigation/Plugins/recastJSPlugin';
+import Recast from 'recast-detour';
+
+// this has to be a require or Node complains
+// const Recast = require('recast-detour');
 
 import '@babylonjs/core/Physics/physicsEngineComponent';
 import '@babylonjs/core/Loading/Plugins/babylonFileLoader';
@@ -20,7 +25,8 @@ import '@babylonjs/loaders';
 // import "@babylonjs/core/Debug/debugLayer";
 // import "@babylonjs/inspector";
 
-import Ragdoll from './ragdoll.js';
+import Ragdoll from './ragdoll';
+import { getClosestAspectRatio } from './utils';
 
 export default async function createScene(engine) {
   const scene = new Scene(engine);
@@ -42,30 +48,88 @@ export default async function createScene(engine) {
 
   // camera
 
-  // const camera = new FreeCamera('camera', new Vector3(0, 4.91, -6), scene);
-  const camera = new FreeCamera('camera', new Vector3(0, 4.91, -18), scene);
+  // const camera = new FreeCamera('camera', new Vector3(0, 4.91, -6), scene); // PERSPECTIVE VERTICAL
+  // const camera = new FreeCamera('camera', new Vector3(0, 4.91, -18), scene); // PERSPECTIVE HORIZONTAL
+  const camera = new FreeCamera('camera', new Vector3(0, 3, 0), scene); // ORTHO HORIZONTAL
+  camera.mode = Camera.ORTHOGRAPHIC_CAMERA;
   camera.fovMode = Camera.FOVMODE_HORIZONTAL_FIXED; // resize the scene based on canvas width instead of height
-  // engine.onResizeObservable.add(() => {
-  //   if (engine.getRenderHeight() > engine.getRenderWidth()) {
-  //       camera.fovMode = Camera.FOVMODE_HORIZONTAL_FIXED;
-  //   }
-  //   else {
-  //       camera.fovMode = Camera.FOVMODE_VERTICAL_FIXED;
-  //   }
-  // })
+  camera.rotation = new Vector3(0.3, 0, 0);
+
+  const [widthRatio, heightRatio] = getClosestAspectRatio(engine.getRenderWidth() / engine.getRenderHeight(), 20);
+  const scaleFactor = 0.5;
+  camera.orthoTop = heightRatio * scaleFactor;
+  camera.orthoBottom = -heightRatio * scaleFactor;
+  camera.orthoLeft = -widthRatio * scaleFactor;
+  camera.orthoRight = widthRatio * scaleFactor;
+
+  const startW = engine.getRenderWidth();
+  const startH = engine.getRenderHeight();
+  engine.onResizeObservable.add(function () {
+    camera.orthoTop = heightRatio * scaleFactor * (engine.getRenderHeight() / startH);
+    camera.orthoBottom = -heightRatio * scaleFactor * (engine.getRenderHeight() / startH);
+    camera.orthoLeft = -widthRatio * scaleFactor * (engine.getRenderWidth() / startW);
+    camera.orthoRight = widthRatio * scaleFactor * (engine.getRenderWidth() / startW);
+  });
 
   // floor
 
-  const floorBoxHeight = 20;
-  const floorBox = MeshBuilder.CreateBox('floor', { width: 40, height: floorBoxHeight, depth: 20 }, scene);
-  floorBox.position.y = -floorBoxHeight / 2;
-  floorBox.physicsImpostor = new PhysicsImpostor(floorBox, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0 }, scene);
+  const floorBox = MeshBuilder.CreateGround('floor', { width: 1, height: 1 }, scene);
+  floorBox.position = new Vector3(0, -1, 0);
+  // floorBox.physicsImpostor = new PhysicsImpostor(floorBox, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0 }, scene);
 
   const floorColor = 128 / 255;
   const floorMaterial = new StandardMaterial('floorMaterial', scene);
   floorMaterial.disableLighting = true;
-  floorMaterial.emissiveColor = new Color3(floorColor, floorColor, floorColor);
+  floorMaterial.emissiveColor = new Color3(floorColor, 0, 0);
   floorBox.material = floorMaterial;
+
+  function resizeGround() {
+    const $footer = document.getElementById('footer');
+    const footerRect = $footer.getBoundingClientRect();
+    const footerCorners = [
+      [footerRect.left, footerRect.top],
+      [footerRect.right, footerRect.top],
+      [footerRect.left, footerRect.bottom],
+      [footerRect.right, footerRect.bottom]
+    ];
+    const [topLeft, topRight, bottomLeft, bottomRight] = footerCorners.map(([x, y]) => Vector3.Unproject(
+      new Vector3(x, y, 0.00001),
+      engine.getRenderWidth(),
+      engine.getRenderHeight(),
+      Matrix.Identity(),
+      camera.getViewMatrix(),
+      camera.getProjectionMatrix()
+    ));
+
+    // floorBox.position = new Vector3(0, -6.37, 0);
+    floorBox.scaling = new Vector3(topRight.x - topLeft.x, 1, -15.39);
+
+    floorBox.refreshBoundingInfo();
+    const floorBoundingInfo = floorBox.getBoundingInfo();
+    floorBoundingInfo.update(floorBox._worldMatrix);
+    const floorBoundingBox = floorBoundingInfo.boundingBox;
+
+    const temp = Vector3.Project(
+      new Vector3(floorBoundingBox.minimumWorld.x, floorBox.position.y, floorBoundingBox.minimumWorld.z),
+      Matrix.IdentityReadOnly,
+      camera.getTransformationMatrix(),
+      camera.viewport.toGlobal(
+        engine.getRenderWidth(),
+        engine.getRenderHeight(),
+      ),
+    );
+    const temp2 = Vector3.Project(
+      new Vector3(floorBoundingBox.minimumWorld.x, floorBox.position.y, floorBoundingBox.maximumWorld.z),
+      Matrix.IdentityReadOnly,
+      camera.getTransformationMatrix(),
+      camera.viewport.toGlobal(
+        engine.getRenderWidth(),
+        engine.getRenderHeight(),
+      ),
+    );
+  }
+  resizeGround();
+  engine.onResizeObservable.add(resizeGround);
 
   // character
 
@@ -121,6 +185,25 @@ export default async function createScene(engine) {
   const ragdoll = new Ragdoll(skeleton, mesh, config, jointCollisions, showBoxes, mainPivotSphereSize, disableBoxBoneSync);
   ragdoll.init();
 
+  // crowd navigation
+
+  const navigationPlugin = new RecastJSPlugin(Recast);
+  navigationPlugin.createNavMesh([floorBox], {
+    cs: 0.2,
+    ch: 0.2,
+    walkableSlopeAngle: 35,
+    walkableHeight: 1,
+    walkableClimb: 1,
+    walkableRadius: 1,
+    maxEdgeLen: 12.,
+    maxSimplificationError: 1.3,
+    minRegionArea: 8,
+    mergeRegionArea: 20,
+    maxVertsPerPoly: 6,
+    detailSampleDist: 6,
+    detailSampleMaxError: 1,
+  });
+
   // resizing logic
 
   // engine.onResizeObservable.add(function () {
@@ -128,8 +211,61 @@ export default async function createScene(engine) {
   //   const screenHeight = engine.getRenderHeight(true);
   // });
 
+  // let screenPos = new Vector3();
+  // let firstRender = true;
+  // let yDistFromBottomInPixels = -1;
+  // scene.onAfterRenderObservable.add(() => {
+  //   if (firstRender) {
+  //     screenPos = Vector3.Project(
+  //       mesh.getAbsolutePosition(),
+  //       Matrix.IdentityReadOnly,
+  //       scene.getTransformMatrix(),
+  //       camera.viewport.toGlobal(
+  //         engine.getRenderWidth(),
+  //         engine.getRenderHeight(),
+  //       ),
+  //     );
+
+  //     yDistFromBottomInPixels = engine.getRenderHeight() - screenPos.y;
+  //     console.log(yDistFromBottomInPixels);
+
+  //     screenPos = screenPos.multiply(new Vector3(1 / engine.getRenderWidth(), 1 / engine.getRenderHeight(), 1));
+
+  //     firstRender = false;
+  //   }
+  // });
+
+  const initialHeight = engine.getRenderHeight();
+  let prevHeight = initialHeight;
+  engine.onResizeObservable.add(() => {
+    if (engine.getRenderHeight() === prevHeight) {
+      return;
+    }
+
+    mesh.position = Vector3.Unproject(
+      new Vector3(engine.getRenderWidth() * 0.25, engine.getRenderHeight() - 100, 0.96), // 0.94455
+      // screenPos.multiply(new Vector3(engine.getRenderWidth(), engine.getRenderHeight(), 1)),
+      engine.getRenderWidth(),
+      engine.getRenderHeight(),
+      Matrix.Identity(),
+      scene.getViewMatrix(),
+      scene.getProjectionMatrix()
+    );
+
+    // floorBox.position = Vector3.Unproject(
+    //   new Vector3(engine.getRenderWidth() * 0.5, engine.getRenderHeight() - 100, 0.95),
+    //   engine.getRenderWidth(),
+    //   engine.getRenderHeight(),
+    //   Matrix.Identity(),
+    //   scene.getViewMatrix(),
+    //   scene.getProjectionMatrix()
+    // );
+
+    prevHeight = engine.getRenderHeight();
+  });
+
   // scene.debugLayer.show({
-  //   overlay: true
+    // overlay: true
   // });
 
   return scene;
