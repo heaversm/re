4d31ff09ed4addd614a7eafb47fba4d61c8c3b0a
Mike Heavers <mheavers@gmail.com>
2021-01-11 17:01:48 -0800

    re3 responsive-ish

--- src/scripts/re3.js
+++ src/scripts/re3.js
@@ -8,6 +8,7 @@ export class RE3 {
     onResizeObserver.add(([containerWidth, containerHeight]) => {
       if (this.p1) {
         this.p1.resizeCanvas(containerWidth, containerHeight);
+        this.p1.handleResizeCanvas(containerWidth, containerHeight);
       }
       if (this.p2) {
         this.p2.resizeCanvas(containerWidth, containerHeight);
@@ -23,8 +24,8 @@ export class RE3 {
     let sketchRenderer;
     let sketch2Renderer;
     const $modal2 = document.getElementById("modal-2");
-    const windowWidth = $modal2.offsetWidth;
-    const windowHeight = $modal2.offsetHeight;
+    let windowWidth = $modal2.offsetWidth;
+    let windowHeight = $modal2.offsetHeight;
 
     const colorSize = 2;
     const gapSize = 8;
@@ -38,13 +39,22 @@ export class RE3 {
 
     const s1 = function (sketch) {
       sketch.setup = function () {
-        squareSize = windowHeight / 2;
         sketchRenderer = sketch.createCanvas(windowWidth, windowHeight);
         sketchRenderer.parent("sketch");
-        numBars = Math.ceil(windowHeight / barSize);
         sketch.frameRate(frameRate);
-
         sketch.clear();
+        sketch.handleSizeCalcs();
+      };
+
+      sketch.handleResizeCanvas = function (cw, ch) {
+        windowWidth = cw;
+        windowHeight = ch;
+        sketch.handleSizeCalcs();
+      };
+
+      sketch.handleSizeCalcs = function () {
+        squareSize = windowHeight / 2;
+        numBars = Math.ceil(windowHeight / barSize);
       };
 
       sketch.draw = function () {
